1acd6043a3f976b3838b7303d8be4c8489f4c427
Sune Simonsen <sune@we-knowhow.dk>
2014-06-18 21:20:06 +0200

    Added initial styling for plain mode

--- test/MagicPen.spec.js
+++ test/MagicPen.spec.js
@@ -5,6 +5,32 @@ var expect = require('unexpected');
 describe('MagicPen', function () {
     var pen;
 
+    describe('in plain mode', function () {
+        beforeEach(function () {
+            pen = new MagicPen();
+        });
+
+        describe('write', function () {
+            describe('when given one argument', function () {
+                it('appends the given text to the output', function () {
+                    pen.write('Hello');
+                    pen.write(' ');
+                    pen.write('world');
+                    expect(pen.toString(), 'to equal', 'Hello world');
+                });
+            });
+
+            describe('when given a style as the first argument', function () {
+                it('appends the given text to the output with the specified styles', function () {
+                    pen.write('red', 'Hello');
+                    pen.write(' ');
+                    pen.write('green', 'world');
+                    expect(pen.toString(), 'to be', 'Hello world');
+                });
+            });
+        });
+    });
+
     describe('in ansi mode', function () {
         beforeEach(function () {
             pen = new MagicPen('ansi');
