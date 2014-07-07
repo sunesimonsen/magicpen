940287caf1ab1bf11352657b25ec44fa94526d59
Sune Simonsen <sune@we-knowhow.dk>
2014-07-07 11:50:19 +0200

    Added Fib example

--- test/MagicPen.spec.js
+++ test/MagicPen.spec.js
@@ -276,5 +276,93 @@ describe('MagicPen', function () {
                    '</code></div></div>\n' +
                    '</code>');
         });
+
+    });
+
+    describe('Fib example', function () {
+        function writeFibWithPen(pen) {
+            pen.addStyle('keyword', function (text) {
+                this.text(text);
+            });
+            pen.addStyle('functionName', function (text) {
+                this.text(text);
+            });
+            pen.addStyle('number', function (text) {
+                this.text(text);
+            });
+            pen.keyword('function').sp().functionName('fib').text(' {')
+                .indentLines()
+                    .indent().keyword('var').text(' i=0, fibs = [').number(0).text(', ').number(1).text('];').nl()
+                    .indent().keyword('for').text(' (; i < n; i += ').number(1).text(') {')
+                    .indentLines()
+                        .indent().text('fibs.push(fibs[').number(0).text('] + fibs[').number(1).text(']);').nl()
+                        .indent().text('fibs.shift();')
+                    .outdentLines()
+                    .indent().text('}').nl()
+                    .indent().keyword('return').text(' fibs[').number(0).text('];')
+                .outdentLines()
+                .text('}');
+        }
+
+        it('in plain mode', function () {
+            var pen = new MagicPen();
+            writeFibWithPen(pen);
+            expect(pen.toString(), 'to equal',
+                   'function fib {\n' +
+                   '  var i=0, fibs = [0, 1];\n' +
+                   '  for (; i < n; i += 1) {\n' +
+                   '    fibs.push(fibs[0] + fibs[1]);\n' +
+                   '    fibs.shift();\n' +
+                   '  }\n' +
+                   '  return fibs[0];\n' +
+                   '}');
+        });
+
+        it('in ansi mode', function () {
+            var pen = new MagicPen('ansi');
+            pen.addStyle('keyword', function (text) {
+                this.text(text, 'blue, bold');
+            });
+            pen.addStyle('functionName', function (text) {
+                this.text(text, 'white, bold');
+            });
+            pen.addStyle('number', function (text) {
+                this.text(text, 'cyan');
+            });
+            writeFibWithPen(pen);
+            expect(pen.toString(), 'to equal',
+                   '\u001b[1m\u001b[34mfunction\u001b[39m\u001b[22m \u001b[1m\u001b[37mfib\u001b[39m\u001b[22m {\n' +
+                   '  \u001b[1m\u001b[34mvar\u001b[39m\u001b[22m i=0, fibs = [\u001b[36m0\u001b[39m, \u001b[36m1\u001b[39m];\n' +
+                   '  \u001b[1m\u001b[34mfor\u001b[39m\u001b[22m (; i < n; i += \u001b[36m1\u001b[39m) {\n' +
+                   '    fibs.push(fibs[\u001b[36m0\u001b[39m] + fibs[\u001b[36m1\u001b[39m]);\n' +
+                   '    fibs.shift();\n' +
+                   '  }\n' +
+                   '  \u001b[1m\u001b[34mreturn\u001b[39m\u001b[22m fibs[\u001b[36m0\u001b[39m];\n' +
+                   '}');
+        });
+
+        it('in html mode', function () {
+            var pen = new MagicPen('html');
+            pen.addStyle('keyword', function (text) {
+                this.text(text, 'black, bold');
+            });
+            pen.addStyle('functionName', function (text) {
+                this.text(text, 'red, bold');
+            });
+            pen.addStyle('number', function (text) {
+                this.text(text, 'cyan');
+            });
+            writeFibWithPen(pen);
+            expect(pen.toString(), 'to equal', '<code>\n' +
+                   '  <div><span style="color: black; font-weight: bold">function</span>&nbsp;<span style="color: red; font-weight: bold">fib</span>&nbsp;{</div>\n' +
+                   '  <div>&nbsp;&nbsp;<span style="color: black; font-weight: bold">var</span>&nbsp;i=0,&nbsp;fibs&nbsp;=&nbsp;[<span style="color: cyan">0</span>,&nbsp;<span style="color: cyan">1</span>];</div>\n' +
+                   '  <div>&nbsp;&nbsp;<span style="color: black; font-weight: bold">for</span>&nbsp;(;&nbsp;i&nbsp;&lt;&nbsp;n;&nbsp;i&nbsp;+=&nbsp;<span style="color: cyan">1</span>)&nbsp;{</div>\n' +
+                   '  <div>&nbsp;&nbsp;&nbsp;&nbsp;fibs.push(fibs[<span style="color: cyan">0</span>]&nbsp;+&nbsp;fibs[<span style="color: cyan">1</span>]);</div>\n' +
+                   '  <div>&nbsp;&nbsp;&nbsp;&nbsp;fibs.shift();</div>\n' +
+                   '  <div>&nbsp;&nbsp;}</div>\n' +
+                   '  <div>&nbsp;&nbsp;<span style="color: black; font-weight: bold">return</span>&nbsp;fibs[<span style="color: cyan">0</span>];</div>\n' +
+                   '  <div>}</div>\n' +
+                   '</code>');
+        });
     });
 });
